50b499b11af1cce1b91d535e069d5bdeb2e1adef
YonisHussein <yonismhussein@gmail.com>
2022-07-31 19:40:27 -0400

    fixing some errors

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,19 @@
-import React, {Component} from "react";
-import {Route, Redirect, HashRouter} from "react-router-dom";
-import About from './About';
+import React  from "react";
+import {HashRouter as Router, Switch, Route, Redirect, HashRouter} from "react-router-dom";
+import About from "./About";
 import Contact from "./Contact";
 import Portfolio from "./Portfolio";
+import Navigation from "./Navigation";
+import { render } from "react-dom";
 
-class Header extends Component {
-    render() {
-        return(
-            <HashRouter>
-                
-            </HashRouter>
-        )
-    }
+function Header(){
+    return (<div>
+
+        
+    </div>)
 }
+
+export default Header;
+
 
 
