57877cef98a1169dbea2650b92d1da4f460c3e40
YonisHussein <yonismhussein@gmail.com>
2022-07-31 16:35:09 -0400

    updated Portfolio and Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,4 +2,16 @@ import React, {Component} from "react";
 import {Route, Redirect, HashRouter} from "react-router-dom";
 import About from './About';
 import Contact from "./Contact";
+import Portfolio from "./Portfolio";
+
+class Header extends Component {
+    render() {
+        return(
+            <HashRouter>
+                
+            </HashRouter>
+        )
+    }
+}
+
 
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import portfolio from '../portfolio.json';
+
+function Portfolio() {
+    return (<div>
+        <h1>Development portfolio: Yonis Hussein</h1>
+        <hr></hr>
+    </div>
+    )
+}
+
+export default Portfolio;
