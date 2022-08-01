693c86b8c1d4c404672996637f6f968c6d339549
YonisHussein <yonismhussein@gmail.com>
2022-07-31 21:04:07 -0400

    fixed the nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,23 @@ import React from "react";
 
 function Navigation(props) {
     const tabs = ['About Me', 'Portfolio', 'Contact'];
-    return ()
+    return (
+        <ul className="header-nav">
+            {tabs.map(tab => (
+                <li className="nav-item" key ={tab}>
+                    <a
+                        href={'#' + tab.toLowerCase()}
+                        onClick={() => props.handlePageChange(tab)}
+                        className={
+                            props.currentPage === tab ? 'nav-link active' : 'nav-link'
+                        }
+                        >
+                            {tab}
+                        </a>
+                </li>
+            ))}
+        </ul>
+    )
 }
 
 export default Navigation;
